e6c2e1525bba4e7197004b0f5b3cd942e6e5bafe
Joao <joaovictorcarmindo@gmail.com>
2022-09-14 15:27:28 -0300

    tela de criar conta*falta responvisidade

--- pages/registrar/app.js
+++ pages/registrar/app.js
@@ -1,17 +1,17 @@
 // selecionando os elementos
 let password = document.getElementById("password");
 let confirm_password = document.getElementById("confirm_password");
-let mensagem_validacao = document.getElementById('validacao')
-
+let mensagem_validacao = document.getElementById("validacao");
 
 // funcao que valida os campos de senha ( input senha e confirmar senha)
-function validarPassword(){
-    if(password.value != confirm_password.value) {
-        // adicionando a class active no paragrafo de aviso
-        mensagem_validacao.classList.add("active");
-
-    } else {
-        // removendo class
-        mensagem_validacao.classList.remove("active");
-    }
+function validarPassword() {
+  if (password.value != confirm_password.value) {
+    // adicionando a class active no paragrafo de aviso
+    mensagem_validacao.classList.add("active");
+  } else {
+    // removendo class
+    mensagem_validacao.classList.remove("active");
+  }
 }
+
+// =============== ANIMAÇÃO INPUT ====================
