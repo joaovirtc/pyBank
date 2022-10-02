7a4cb0d9578deefdd8974b8c1d922b33dd993010
Joao <joaovictorcarmindo@gmail.com>
2022-10-02 17:05:33 -0300

    frontend finalizado

--- pages/user/app.js
+++ pages/user/app.js
@@ -1,16 +1,13 @@
 let form = document.getElementById("form");
 
-
-
-
-
-
-
 function toggle() {
-    var blur = document.getElementById("blur")
-    blur.classList.toggle("active")
-    var popup = document.getElementById("popup")
-    popup.classList.toggle("active")
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("active");
 }
 
-
+function toggle2() {
+  var popup2 = document.getElementById("popup2");
+  popup2.classList.toggle("active");
+}
