dfacde8e0b73e879a558bb12ed5d8c6931f8d95b
Joao <joaovictorcarmindo@gmail.com>
2022-10-08 15:38:24 -0300

    alterações no login, criar conta e checkout

--- pages/registrar/checkout.php
+++ pages/registrar/checkout.php
@@ -1,45 +1,51 @@
-<?php
+<!-- <?php
     session_start();
-?>
+?> -->
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="checkout.css">
     <title>checkout</title>
 </head>
+
 <body>
     <main>
         <header>
-            <a href="../../index.html"><img src="../8.png" alt="LOGO"></a>
+            <a href="../../index.html"><img src="../../assets/img/logoWhite.png" alt="LOGO"></a>
         </header>
+
+
         <div class="container__form">
             <form action="cod_verificar.php" method="POST">
-            <div class="text-form">
+                <div class="text-form">
                     <h2>CÓDIGO DE VERIFICAÇÃO</h2>
                     <p>Acabamos de enviar um código no seu e-mail
                         cheque e o digite abaixo</p>
-            </div>
-            <div class="single-input">
+                </div>
+                <div class="single-input">
                     <input type="text" name="codigo_user" class="input" required>
                     <label for="codigo_user">Digite o código</label>
-            </div>
-            <div class="links">
-                    <input type="submit" class="primario" value="Enviar" name="enviar">
+                </div>
+                <div class="links">
+                    <a href="../../index.html" class="terciario">Enviar código novamente</a>
+                    <input type="submit" class="primario" value="Conferir código" name="enviar">
                     <a href="../../index.html" class="secundario">Voltar</a>
-                    <a href="../../index.html" class="secundario">Enviar novamente</a>
-            </div>
+                </div>
+            </form>
 
 
         </div>
     </main>
 
 </body>
+
 </html>
 
-<?php
+<!-- <?php
     $nome = $_SESSION['nome'];
     $email = $_SESSION['email'];
     $senha = $_SESSION['senha'];
@@ -68,4 +74,4 @@
     };
     // volta para o registro
 
-?>
+?> -->
